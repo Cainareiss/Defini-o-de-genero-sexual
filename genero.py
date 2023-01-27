29bf21a13d837976abdb72ff59cc12bd2c1550dc
Cainã Reis <106223762+Cainareiss@users.noreply.github.com>
2023-01-27 09:34:02 -0300

    Update genero.py

--- genero.py
+++ genero.py
@@ -1,10 +1,10 @@
 pergunta = str(input("Digite M para masculino e F para Feminino:"))
-processamento="m" or "M" or 'f' or 'F'
+#processamento='m' or 'M' or 'f' or 'F'
 if pergunta in ['m', 'M','f','F']:
     print("Sexo valido")
 else:
-    print('sexo invalido')
-erro1 = str(input("Digite novamente:"))
-if erro1 in ['m', 'M','f','F']:
+    #print('sexo invalido')
+    E = str(input("Digite novamente:"))
+if E in ['m', 'M','f','F']:
     print("Sexo valido")
 exit()
